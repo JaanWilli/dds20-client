e17718d08209c668f6e4ea93ca3cbc4811a818c5
regloff <remy.egloff@flashcable.ch>
2020-10-08 11:35:06 +0200

    Changed webpage title

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { BaseContainer } from "../../helpers/layout";
-import { api, handleError } from "../../helpers/api";
+import { BaseContainer } from "../helpers/layout";
+import { api, handleError } from "../helpers/api";
 import User from "../shared/models/User";
 import { Button } from "../../views/design/Button";
 
